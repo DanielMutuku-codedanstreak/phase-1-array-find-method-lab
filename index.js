fad5ccb3eb07ba12f71cdb29d4991315954684db
Daniel Mutuku <danielmakau507@gmail.com>
2023-10-01 08:04:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+// Check if a win was found and return the corresponding year otherwise return undefined
+function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+  
+    return winningRecord ? winningRecord.year : undefined;
+}
